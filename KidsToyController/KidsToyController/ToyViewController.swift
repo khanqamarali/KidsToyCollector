dc7d4eec1c9118b4c549e872bd2dc5dd791b7685
qamarali <khanqamarali@gmail.com>
2017-06-12 11:42:30 -0500

    Added all wiring around Toy Image Control view.

---
+++ KidsToyController/KidsToyController/ToyViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ToyViewController.swift
+//  KidsToyController
+//
+//  Created by qamarali on 6/12/17.
+//  Copyright © 2017 qamarali. All rights reserved.
+//
+
+import UIKit
+
+class ToyViewController: UIViewController {
+
+    @IBOutlet weak var ToyImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TitleTextField: UITextField!
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func CameraTapped(_ sender: Any) {
+    }
+
+    @IBAction func PhotoTapped(_ sender: Any) {
+    }
+    
+    @IBAction func AddTapped(_ sender: Any) {
+    }
+}
